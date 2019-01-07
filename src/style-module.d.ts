71cc6e866976cbe2e73a6d1d09677db38e1b51f4
Marijn Haverbeke <marijn@haverbeke.nl>
2019-01-07 14:14:19 +0100

    Add TS definition file

---
+++ src/style-module.d.ts
@@ -0,0 +1,13 @@
+export enum Priority {}
+
+export class StyleModule {
+  constructor(classes: {[name: string]: Style});
+  mount(root: Document | ShadowRoot, priority?: Priority): {[name: string]: string};
+  static lowPriority: Priority;
+  static normalPriority: Priority;
+  static highPriority: Priority;
+}
+
+export type Style = {
+  [propOrSelector: string]: string | number | Style
+}
