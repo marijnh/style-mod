2556663165b413c9da82c48168cec60bea171ae3
Marijn Haverbeke <marijn@haverbeke.nl>
2019-01-07 15:32:31 +0100

    Make type of root parameter to mount a little more ridiculous

--- src/stylemodule.d.ts
+++ src/stylemodule.d.ts
@@ -2,7 +2,7 @@ export enum Priority {}
 
 export class StyleModule {
   constructor(classes: {[name: string]: Style});
-  mount(root: Document | ShadowRoot, priority?: Priority): {[name: string]: string};
+  mount(root: Document | ShadowRoot | DocumentOrShadowRoot, priority?: Priority): {[name: string]: string};
   static lowPriority: Priority;
   static normalPriority: Priority;
   static highPriority: Priority;
