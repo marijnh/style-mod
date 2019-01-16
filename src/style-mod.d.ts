b20bc052dc5175015e931eb24f198a3998f6891f
Adrian Heine né Lang <mail@adrianheine.de>
2019-01-16 20:23:41 +0100

    Simplify type declarations

--- src/style-mod.d.ts
+++ src/style-mod.d.ts
@@ -1,7 +1,7 @@
 export type StyleModule<S = {}> = {[id in keyof S]: string}
 
 export const StyleModule: {
-  new <S extends {[name: string]: Style}>(spec: S, options?: {priority?: 0 | 1 | 2}): StyleModule<{[id in keyof S]: string}>
+  new <S extends {[name: string]: Style}>(spec: S, options?: {priority?: 0 | 1 | 2}): StyleModule<S>
   mount(root: Document | ShadowRoot | DocumentOrShadowRoot, module: StyleModule): void
 }
 
