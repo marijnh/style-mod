2c7c093ab782f3a1712c9d35540f3f34627317ae
Marijn Haverbeke <marijn@haverbeke.nl>
2019-01-16 13:23:20 +0100

    Simplify displayed types of style modules
    (So you don't get the whole expanded mess in your type hints and error
    messages.)

--- src/stylemodule.d.ts
+++ src/stylemodule.d.ts
@@ -1,4 +1,4 @@
-export function styleModule<S extends {[name: string]: Style}>(spec: S, options?: {priority?: 0 | 1 | 2}): StyleModule<S>
+export function styleModule<S extends {[name: string]: Style}>(spec: S, options?: {priority?: 0 | 1 | 2}): StyleModule<{[id in keyof S]: string}>
 
 export declare namespace styleModule {
   export function mount(root: Document | ShadowRoot | DocumentOrShadowRoot, module: StyleModule): void
